6cbb9c665dcf625bda9383153a6f32f35e11143c
Sergey Leshkevich <s.leshkevichemail@gmail.com>
2024-03-04 10:11:34 +0300

    add NewsRequest class

---
+++ src/main/java/ru/clevertec/news/entity/dto/NewsRequest.java
@@ -0,0 +1,18 @@
+package ru.clevertec.news.entity.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record NewsRequest(
+        @NotBlank
+        @Size(min = 1, max = 50)
+        String title,
+
+        @NotBlank
+        @Size(min = 1, max = 2000)
+        String text,
+
+        @NotNull
+        UserRequest user) {
+}
