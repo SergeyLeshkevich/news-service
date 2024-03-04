b139505b58737980e6c2b70989e35c3f6f3b4067
Sergey Leshkevich <s.leshkevichemail@gmail.com>
2024-03-04 11:12:47 +0300

    add resorces for tests

---
+++ src/test/resources/db/changelog/data/v1-insert-changeset-users-table-test.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (uuid,user_name)
+VALUES ('0bdc4d34-af90-4b42-bba6-f588323c87d7','Test userName comment')
---
+++ src/test/resources/db/changelog/data/v1-insert-news-table-test.sql
@@ -0,0 +1,5 @@
+INSERT INTO news (title, time, text,is_archive,user_id)
+VALUES ('Test news title', '2024-01-01 14:18:08.537000', 'Test news text',false,1),
+       ('Test news title2', '2024-01-01 14:18:08.537000', 'Test news text2',false,1),
+       ('Test news title', '2024-01-01 14:18:08.537000', 'Test news text',true,1),
+       ('Test news title2', '2024-01-01 14:18:08.537000', 'Test news text',true,1);
