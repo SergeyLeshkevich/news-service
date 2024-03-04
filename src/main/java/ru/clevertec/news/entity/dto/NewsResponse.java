40632c62c3f17b111f63a2d2d41e30cdf95a9613
Sergey Leshkevich <s.leshkevichemail@gmail.com>
2024-03-04 10:11:53 +0300

    add NewsResponse class

---
+++ src/main/java/ru/clevertec/news/entity/dto/NewsResponse.java
@@ -0,0 +1,20 @@
+package ru.clevertec.news.entity.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import ru.clevertec.news.entity.User;
+
+import java.time.LocalDateTime;
+
+public record NewsResponse(
+
+        Long id,
+
+        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
+        LocalDateTime time,
+
+        String title,
+
+        String text,
+
+        User user) {
+}
